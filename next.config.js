84c5e1951226d6396294178cef4ca6b18d3b1b0a
Zeeshan Ahmad Khalil <41861952+ZeeshanAhmadKhalil@users.noreply.github.com>
2023-02-08 11:14:07 +0500

    created new TS template

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      loader: '@svgr/webpack',
-      options: {
-        prettier: false,
-        svgo: true,
-        titleProp: true,
-        esModule: false,
-      },
-      issuer: {
-        and: [/\.(js|ts)x?$/],
-      },
-      test: /\.svg$/,
-    });
-
-    return config;
-  },
-  reactStrictMode: false,
-  images: {
-    loader: 'akamai',
-    path: '/',
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
